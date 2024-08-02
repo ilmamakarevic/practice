ef18a66302cd9f3bf8770baef2b762cfaa369fc3
Ilma Makarevic <makarevic85@gmail.com>
2024-08-02 13:13:56 +0200

    fixed form submission

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function validateRegistration() {
-    let x = document.getElementById("fname", "lname", "email", "password").value;
-    if(x==""){
-        alert("You must enter your name"); //ne radi
+    let x = document.forms["myForm"];
+    let name = x["fname"].value;
+    let surname = x["lname"].value;
+    let email = x["email"].value;
+    let password = x["password"].value;
+
+    if(name == "" || surname == "" || email == "" || password == ""){
+        alert("Make sure you have filled in all the fields.");
         return false;
     }
+    else{
+        alert("You have successfully subscribed to our newsletter! ")
+    }
 }
+
